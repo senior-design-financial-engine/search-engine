0f888a04b3d499c23c2f5a2ba0c2f2da0d655aa6
Karlnaba <113692955+Karlnaba@users.noreply.github.com>
2024-11-01 16:34:28 -0400

    Front end web app started
    Added App.js along with two components which are Home.js and Results.js

---
+++ App.js
@@ -0,0 +1,20 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Results from './components/Results';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
